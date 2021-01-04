ffccd3fe303b17ae3c47a6a0cf512322074ba00d
Milagros Rassl <milagros.rassl@gmail.com>
2021-01-04 02:52:26 -0300

    Soporte a idiomas y orientación funcionando

---
+++ app/src/main/java/com/mrassl/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mrassl.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String languageToLoad = "es/strings";
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+        this.setContentView(R.layout.activity_main);
+    }
+}
